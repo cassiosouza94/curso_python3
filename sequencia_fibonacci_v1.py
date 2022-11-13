860db6731cd22257f94e267b447c1ac5aa4319b8
cassioasouzaa <cassioasouzaa@gmail.com>
2022-11-13 12:25:24 -0300

    O código da Sequência de Fibonacci utilizando um parâmetro de controle dentro da função.

---
+++ sequencia_fibonacci_v1.py
@@ -0,0 +1,14 @@
+def fibonacci(limitation_on_count):
+
+    penultimo_numero = 0
+    ultimo_numero = 1
+    print(f'{penultimo_numero}, {ultimo_numero}', end='-')
+    while ultimo_numero < limitation_on_count:
+        proximo_numero = penultimo_numero + ultimo_numero
+        print(f'{proximo_numero}', end='-')
+        penultimo_numero = ultimo_numero
+        ultimo_numero = proximo_numero
+
+
+if __name__ == '__main__':
+    fibonacci(20000)
