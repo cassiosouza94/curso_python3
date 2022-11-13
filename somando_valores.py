ae924a860cfdd838d26c2b178659659a7c3c8e7d
cassioasouzaa <cassioasouzaa@gmail.com>
2022-11-13 12:04:02 -0300

    Mais um código em Python bem bacana utilizando o input.

---
+++ somando_valores.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite o primeiro número de sua escolha : '))
+n2 = int(input('Digite o segundo número de sua escolha : '))
+soma = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
