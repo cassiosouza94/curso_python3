929723d7d74e6c2dd7dcbba8ab943d33d83cdb41
cassioasouzaa <cassioasouzaa@gmail.com>
2022-11-13 14:39:37 -0300

    ...

--- sequencia_fibonacci_v1.py
+++ sequencia_fibonacci_v1.py
@@ -2,10 +2,10 @@ def fibonacci(limitation_on_count):
 
     penultimo_numero = 0
     ultimo_numero = 1
-    print(f'{penultimo_numero}, {ultimo_numero}', end='-')
+    print(f'{penultimo_numero}, {ultimo_numero}', end=',')
     while ultimo_numero < limitation_on_count:
         proximo_numero = penultimo_numero + ultimo_numero
-        print(f'{proximo_numero}', end='-')
+        print(f'{proximo_numero}', end=',')
         penultimo_numero = ultimo_numero
         ultimo_numero = proximo_numero
 
