1be2d98edaafdfd3f0d526519dc51760d378f94f
cassioasouzaa <cassioasouzaa@gmail.com>
2022-11-20 12:03:17 -0300

    Código testado e funcionando 100% sem falhas.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from cnpj import Cnpj
 
 
 sistema_validador_CPF = Cpf()
-print(sistema_validador_CPF.valido('11122233396'))
+print(sistema_validador_CPF.valido('111.222.333-96'))
 
 sistema_validador_CNPJ = Cnpj()
 print(sistema_validador_CNPJ.valido('88.458.835/0001-30'))
